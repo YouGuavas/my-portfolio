aded6201e2dc1b8682aab74dcd65bc7ef5e5b662
Patrick Yambrick <youguavas@Patricks-MacBook-Pro.local>
2019-02-27 18:03:06 -0500

    Separate projects component into its own file

---
+++ src/components/Projects.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+export default class Projects extends Component {
+
+	render() {
+		return(
+			<section className='page-section projects' id='projects'>
+				<h1>Projects</h1>
+				<div className="project-container">
+					<Project />
+					<Project />
+				</div>
+			</section>
+		)
+	}
+}
+
+class Project extends Component {
+	render() {
+		return(
+			<div className='project'>
+				<img src='' />
+				<p></p>
+				<a href="#" className="icon-anchor icon-anchor-1"><FontAwesomeIcon icon={['fab', 'github']} /> View on Github</a>
+				<a href="#" className='icon-anchor icon-anchor-2'>View Site</a>
+			</div>
+		)
+	}
+}

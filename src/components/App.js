c91d899e4a4c217f336fac6ac6bd75c7c692dad6
Patrick Yambrick <patrickyambrick@gmail.com>
2019-08-06 09:55:57 -0400

    Add Resume link

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,11 @@ import Projects from './Projects';
 import Contact from './Contact';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import {faHome, faAddressBook, faProjectDiagram} from '@fortawesome/free-solid-svg-icons';
+import {fas, faHome, faAddressBook, faProjectDiagram, faFile} from '@fortawesome/free-solid-svg-icons';
 import ReactTypingEffect from 'react-typing-effect';
 
 
-library.add(fab, faHome, faAddressBook, faProjectDiagram);
+library.add(fab, fas, faHome, faAddressBook, faProjectDiagram, faFile);
 
 
 export default class App extends Component {
@@ -29,7 +29,7 @@ class Main extends Component {
 	render() {
 		return(
 			<section className='jumbo page-section'>
-				<Nav links={{brand: ["Patrick", "#"], other: [["Things I've made", "#projects", "project-diagram"], ["Contact Me", "#contact", "address-book"]]}} />
+				<Nav links={{brand: ["Patrick", "#"], other: [["Things I've made", "#projects", "project-diagram"], ["Contact Me", "#contact", "address-book"], ["Resume", "https://docs.google.com/document/d/1-6zsvdHRcdH4NCc-BsV-84rT-9BHHgoZb3pBNzLAXac/edit?usp=sharing", "file"]]}} />
 				<div className="main-grid">
 				<div className='container main'>
 					<h1>Patrick Yambrick</h1>
